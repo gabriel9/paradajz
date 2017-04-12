6f8ec5deea337a72be9e6c22b123748c9495c9aa
Bojan Kostic <blood9raven@gmail.com>
2017-04-13 00:56:46 +0200

    Added more logic to DbWrapper.js CRUD for tasks. Added more setup statements.

--- qml/DbWrapper.js
+++ qml/DbWrapper.js
@@ -4,24 +4,37 @@
 
 
 
-var test = (function() {
+var dbInterface = (function() {
     var db = Sql.LocalStorage.openDatabaseSync("ParadajzDB", "1.0", "Paradajz DB.", 1000000)
     console.log("Init lib!")
     db.transaction(function(tx) {
         tx.executeSql("CREATE TABLE IF NOT EXISTS settings(rule_name TEXT, value INTEGER)")
+        tx.executeSql("CREATE TABLE IF NOT EXISTS tasks(task_name TEXT, task_description TEXT, task_status INTEGER)")
 
-        //TODO: If there is no values in settings populate it
-
+        var settingRows = tx.executeSql("SELECT * FROM settings")
+        if(settingRows.rows.length === 0) {
+            tx.executeSql("INSERT INTO settings (rule_name, value) VALUES ('shortBreak', 5), ('longBreak', 10), ('taskTime', 25)")
+        }
     })
 
+     var insertNewTask = function(taskName, taskDescription) { // TODO: add return
+        db.transaction(function(tx) {
+            tx.executeSql("INSERT INTO tasks (task_name, task_description, task_status), VALUES(?, ?, ?)", taskName, taskDescription, 0)
+        })
+    }
 
+    var updateTask = function(taskId, taskName, taskDescription, taskStatus) { // TODO: add return
+        db.transaction(function(tx) {
+            tx.executeSql("UPDATE tasks SET task_name = ?, task_description = ?, task_status = ? WHERE taskId = ?", taskName, taskDescription, taskStatus, taskStatus)
+        })
+    }
 
-     var func1 = function() {
-        console.log("func1!")
+    var deleteTask = function(taskId) { // TODO: add return
+        db.transaction("DELETE FROM tasks WHERE task_id = ?", taskId)
     }
 
     return {
-    func1: func1
+    insertNewTask: insertNewTask
     }
 })()
 
